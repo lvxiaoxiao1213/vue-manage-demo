6990b3f69232794e6190a897d9afb176d90b47dc
Xiaoxiao Lv(Vicky) <xiaoxiaolv@hengtiansoft.com>
2019-04-29 15:37:47 +0800

    added the tendency.vue with echart

--- src/page/home.vue
+++ src/page/home.vue
@@ -1,21 +1,25 @@
 <template>
     <div>
         <head-top></head-top>
+        <tendency :sevenDate="sevenDate" :sevenDay="sevenDay"></tendency>
     </div>
 </template>
 
 <script>
 import headTop from '@/components/headTop'
+import tendency from '@/components/tendency'
 
 export default {
     name: 'Home',
     data() {
         return {
-
+            sevenDay: [],
+            sevenDate: [[], [], []]
         }
     },
     components: {
-        headTop
+        headTop,
+        tendency
     }
 }
 </script>
